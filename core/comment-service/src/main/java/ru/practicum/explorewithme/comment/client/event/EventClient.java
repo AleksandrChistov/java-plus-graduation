8fe0384f545eea57b795445cde84ced92f669376
Alexander Chistov <aleksandrchistov.ru@yandex.ru>
2025-12-17 19:28:23 +0700

    fix EventClient

--- core/comment-service/src/main/java/ru/practicum/explorewithme/comment/client/event/EventClient.java
+++ core/comment-service/src/main/java/ru/practicum/explorewithme/comment/client/event/EventClient.java
@@ -2,7 +2,8 @@ package ru.practicum.explorewithme.comment.client.event;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import ru.practicum.explorewithme.api.event.service.EventServiceApi;
+import ru.practicum.explorewithme.comment.config.EventClientConfig;
 
-@FeignClient(name = "event-service", configuration = EventClientErrorDecoder.class, fallback = EventClientFallback.class)
+@FeignClient(name = "event-service", configuration = EventClientConfig.class, fallback = EventClientFallback.class)
 public interface EventClient extends EventServiceApi {
 }
--- core/request-service/src/main/java/ru/practicum/explorewithme/request/client/event/EventClient.java
+++ core/request-service/src/main/java/ru/practicum/explorewithme/request/client/event/EventClient.java
@@ -2,7 +2,8 @@ package ru.practicum.explorewithme.request.client.event;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import ru.practicum.explorewithme.api.event.service.EventServiceApi;
+import ru.practicum.explorewithme.request.config.EventClientConfig;
 
-@FeignClient(name = "event-service", configuration = EventClientErrorDecoder.class, fallback = EventClientFallback.class)
+@FeignClient(name = "event-service", configuration = EventClientConfig.class, fallback = EventClientFallback.class)
 public interface EventClient extends EventServiceApi {
 }
