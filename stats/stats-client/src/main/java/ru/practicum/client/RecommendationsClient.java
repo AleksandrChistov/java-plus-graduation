85f37a2bbe035ceaebe42e2968cac756b78316e3
Alexander Chistov <aleksandrchistov.ru@yandex.ru>
2026-01-08 20:12:44 +0700

    create RecommendationsClient

---
+++ stats/stats-client/src/main/java/ru/practicum/client/RecommendationsClient.java
@@ -0,0 +1,60 @@
+package ru.practicum.client;
+
+import net.devh.boot.grpc.client.inject.GrpcClient;
+import org.springframework.stereotype.Component;
+import ru.practicum.ewm.stats.proto.*;
+
+import java.util.Iterator;
+import java.util.Set;
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+@Component
+public class RecommendationsClient {
+
+    @GrpcClient("analyzer")
+    private RecommendationsControllerGrpc.RecommendationsControllerBlockingStub analyzerClient;
+
+    public Stream<RecommendedEventProto> getSimilarEvents(long eventId, long userId, int maxResults) {
+        SimilarEventsRequestProto request = SimilarEventsRequestProto.newBuilder()
+                .setEventId(eventId)
+                .setUserId(userId)
+                .setMaxResults(maxResults)
+                .build();
+
+        Iterator<RecommendedEventProto> iterator = analyzerClient.getSimilarEvents(request);
+
+        return asStream(iterator);
+    }
+
+    public Stream<RecommendedEventProto> getRecommendationsForUser(long userId, int maxResults) {
+        UserPredictionsRequestProto request = UserPredictionsRequestProto.newBuilder()
+                .setUserId(userId)
+                .setMaxResults(maxResults)
+                .build();
+
+        Iterator<RecommendedEventProto> iterator = analyzerClient.getRecommendationsForUser(request);
+
+        return asStream(iterator);
+    }
+
+    public Stream<RecommendedEventProto> getInteractionsCount(Set<Long> eventIds, int maxResults) {
+        InteractionsCountRequestProto request = InteractionsCountRequestProto.newBuilder()
+                .addAllEventId(eventIds)
+                .build();
+
+        Iterator<RecommendedEventProto> iterator = analyzerClient.getInteractionsCount(request);
+
+        return asStream(iterator);
+    }
+
+    private Stream<RecommendedEventProto> asStream(Iterator<RecommendedEventProto> iterator) {
+        return StreamSupport.stream(
+                Spliterators.spliteratorUnknownSize(iterator, Spliterator.ORDERED),
+                false
+        );
+    }
+
+}
